f7ec0573d23cc652e8986d0ff534de3a87faca6c
Ashay Tamrakar <48744162+ashayatd@users.noreply.github.com>
2023-09-15 17:57:32 +0530

    fontawesome remove

--- course-management/src/App.js
+++ course-management/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Routes from './routes/RoutesPages';
 import { AuthProvider } from '../../course-management/src/authentication/AuthContext';
-import "@fortawesome/fontawesome-free/css/all.min.css";
 
 
 function App() {
