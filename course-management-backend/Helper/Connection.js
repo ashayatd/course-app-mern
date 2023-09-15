73f0cbbc11443c1356460d4391c6ff7708bab9a8
Ashay Tamrakar <48744162+ashayatd@users.noreply.github.com>
2023-09-15 17:19:41 +0530

    fixing db name

--- course-management-backend/Helper/Connection.js
+++ course-management-backend/Helper/Connection.js
@@ -2,12 +2,13 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 
+DB = process.env.DB_NAME;
 USERNAME = process.env.DB_USERNAME;
 PASSWORD = process.env.DB_PASSWORD;
 CLUSTERURL = process.env.CLUSTERURL;
 
 function connection() {
-  const MONGODB_URI = `mongodb+srv://${USERNAME}:${PASSWORD}@${CLUSTERURL}/courses_app`;
+  const MONGODB_URI = `mongodb+srv://${USERNAME}:${PASSWORD}@${CLUSTERURL}/${DB}`;
   mongoose.connect(MONGODB_URI);
 
   mongoose.connection.on("connected", () => {
